f775f8bc50dc4a222ea0145f80356c2de978a78d
Guilherme Hilst Ribeiro <guilhermehilst@gmail.com>
2018-09-21 16:34:47 -0300

    update route create and add route show on races_controller

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -6,7 +6,19 @@ class RacesController < ApplicationController
   def create
     @race = Race.new(race_params)
 
-    @race.save
+    ActiveRecord::Base.transaction do
+      if @race.save
+        Race::Parser.call(params[:file], @race)
+      end
+    end
+
+    redirect_to @race
+  end
+
+  def show
+    @race = Race.find(params[:id])
+    @racer_infos = @race.racer_infos.order(:total_time)
+    @best_lap = @race.racer_infos.order(:time_best_lap).first
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'races#index'
 
-  post 'races', to: 'races#create'
+  resources :races, only: [:index, :show, :create]
 end
