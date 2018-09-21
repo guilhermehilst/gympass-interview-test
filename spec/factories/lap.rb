bc6c78562cf09eb69a2a7cab52684009cf55db8a
Guilherme Hilst Ribeiro <guilhermehilst@gmail.com>
2018-09-21 11:50:50 -0300

    Add gem factory_bot_rails and create factories for lap race and racer

---
+++ spec/factories/lap.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :lap do
+    number { 1 }
+  end
+end
---
+++ spec/factories/racer.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :racer do
+    name { "Guilherme Hilst" }
+    code { 10 }
+  end
+end
